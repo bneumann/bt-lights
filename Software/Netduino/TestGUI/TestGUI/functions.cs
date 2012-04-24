99ee9ed6d2db8d6e07b9b34a1bd7d35a98f7038c
neumann.benni@gmail.com <neumann.benni@gmail.com@8ac5c010-e867-c397-7d37-eeac77e9b270>
2012-04-24 23:10:33 +0000

    Fixed some remaining conflicts

---
+++ Software/Netduino/TestGUI/TestGUI/functions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class functions
+    {
+        public static double GetMax(double[] DoubleCollection)
+        {
+            double max = double.MinValue;
+            foreach (double i in DoubleCollection)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        public static int GetMax(int[] DoubleCollection)
+        {
+            int max = int.MinValue;
+            foreach (int i in DoubleCollection)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+    }
+}
