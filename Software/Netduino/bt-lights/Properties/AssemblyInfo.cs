91bc7f5b8ba3dabfebe69a508e85cdac9a606631
neumann.benni@gmail.com <neumann.benni@gmail.com@8ac5c010-e867-c397-7d37-eeac77e9b270>
2013-03-28 17:59:57 +0000

    UPDATE:  - Added assembly information for official release 1.0

--- Software/Netduino/bt-lights/Properties/AssemblyInfo.cs
+++ Software/Netduino/bt-lights/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SPITest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Meister Lampe")]
+[assembly: AssemblyDescription("Firmware for Meister Lampe")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SPITest")]
+[assembly: AssemblyCompany("BN Robotics")]
+[assembly: AssemblyProduct("Meister Lampe FW")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
